d1c0647713e0737f4e447149eab54dc44cc0ea36
João Vitor <joaovitortgoliveira@gmail.com>
2023-09-22 11:31:30 -0300

    Commit_2

--- src/main/java/com/example/task/repositories/TaskRepository.java
+++ src/main/java/com/example/task/repositories/TaskRepository.java
@@ -2,8 +2,11 @@ package com.example.task.repositories;
 
 import com.example.task.models.TaskModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface TaskRepository extends JpaRepository<TaskModel, UUID> {
+
 }
