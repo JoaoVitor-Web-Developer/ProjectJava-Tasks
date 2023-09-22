88293a897f34903531acec8c7be7fd09438a074b
João Vitor <joaovitortgoliveira@gmail.com>
2023-09-21 21:15:18 -0300

    commit_2

---
+++ src/main/java/com/example/task/models/TaskModel.java
@@ -0,0 +1,56 @@
+package com.example.task.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_TASKS")
+public class TaskModel implements Serializable {
+    private  static  final  long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private UUID idTask;
+    @Column
+    private String tasks;
+    @Column
+    private String responsible;
+    @Column
+    private String observation;
+
+    public UUID getIdTask() {
+        return idTask;
+    }
+
+    public String getTasks() {
+        return tasks;
+    }
+
+    public String getResponsible() {
+        return responsible;
+    }
+
+    public String getObservation() {
+        return observation;
+    }
+
+
+    public void setIdTask(UUID idTask) {
+        this.idTask = idTask;
+    }
+
+    public void setTasks(String tasks) {
+        this.tasks = tasks;
+    }
+
+    public void setResponsible(String responsible) {
+        this.responsible = responsible;
+    }
+
+    public void setObservation(String observation) {
+        this.observation = observation;
+    }
+}
